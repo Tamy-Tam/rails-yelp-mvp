dcc25ba945f52d48f937a7f4ac9971a3f240c973
Tamara Robson <tamara.jade.robson@gmail.com>
2021-03-03 14:42:44 +0100

    Models ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+bristol = Restaurant.new(name: "Epicure", address:"3 rue des arts", category:"french")
+love = Restaurant.new(name: "Chez Mamie", address:"45 Avenue des Lpinsa", category:"italian")
+encore = Restaurant.new(name: "Java", address:"2 boulvard Carnot", category:"belgian")
+restau = Restaurant.new(name: "Le Lointain", address:"56 rue des Lilas", category:"japanese")
+ile = Restaurant.new(name: "Ile", address:"Londre, RU", category:"chinese")
