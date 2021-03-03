1ab43f1dbb91ae4b0b67c9fc8fc0883ef262b8dc
Tamara Robson <tamara.jade.robson@gmail.com>
2021-03-03 16:04:17 +0100

    Controllers almost done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,42 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new(params[:restaurant])
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :restaurants
+
+  # get '/restaurants', to: 'restaurants#index'
+  # post '/restaurants', to: 'restaurants#create'
+  # get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # get '/restaurants/:id', to: "restaurants#show", as: :restaurant
+
+   resources :restaurants, only: [:create, :index, :new, :show] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
